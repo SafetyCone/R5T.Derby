56925beb22c9968b8c3965ede3955558439c66b2
MinexAutomation <minex.automation@gmail.com>
2019-12-08 23:04:07 -0800

    Added add custom Rivet user secrets file to configuration builder extension.

--- source/R5T.Derby/Code/Extensions/IConfigurationBuilderExtensions.cs
+++ source/R5T.Derby/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 using ShrewsburyFileNames = R5T.Shrewsbury.FileNames;
+
+using R5T.Suebia;
 
 
 namespace R5T.Derby.Extensions
@@ -10,12 +13,22 @@ namespace R5T.Derby.Extensions
     public static class IConfigurationBuilderExtensions
     {
         /// <summary>
-        /// Adds:
-        /// * <see cref="ShrewsburyFileNames.DefaultAppSettingsJsonFileName"/>.
+        /// Adds the default appsettings.json file.
         /// </summary>
         public static IConfigurationBuilder AddDefaultAppSettingsJsonFile(this IConfigurationBuilder configurationBuilder)
         {
             configurationBuilder.AddJsonFile(ShrewsburyFileNames.DefaultAppSettingsJsonFileName);
+
+            return configurationBuilder;
+        }
+
+        public static IConfigurationBuilder AddRivetUserSecretsFile(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider, string secretsFileName)
+        {
+            var secretsFilePathProvider = configurationServiceProvider.GetRequiredService<ISecretsFilePathProvider>();
+
+            var secretsFilePath = secretsFilePathProvider.GetSecretsFilePath(secretsFileName);
+
+            configurationBuilder.AddJsonFile(secretsFilePath);
 
             return configurationBuilder;
         }
