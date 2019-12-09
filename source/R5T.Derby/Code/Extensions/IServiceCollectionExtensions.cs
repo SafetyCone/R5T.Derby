19d2ecc0d39698ccd7b3c58024c80e09f48cbade
MinexAutomation <minex.automation@gmail.com>
2019-12-08 22:41:35 -0800

    Added configuration-name to appsettings file name token converter.

--- source/R5T.Derby/Code/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Derby/Code/Extensions/IServiceCollectionExtensions.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Ives;
 using R5T.Ives.Configuration;
+using R5T.Shrewsbury;
+using R5T.Shrewsbury.Default;
 
 
 namespace R5T.Derby.Extensions
@@ -11,13 +13,13 @@ namespace R5T.Derby.Extensions
     public static class IServiceCollectionExtensions
     {
         /// <summary>
-        /// Adds:
-        /// * <see cref="IConfigurationNameProvider"/>.
+        /// Adds services to get a configuration name-specific app-settings file name.
         /// </summary>
         public static IServiceCollection AddDirectConfigurationBasedConfigurationNameProvider(this IServiceCollection services)
         {
             services
                 .AddSingleton<IConfigurationNameProvider, DirectConfigurationBasedConfigurationNameProvider>()
+                .AddSingleton<IConfigurationNameToAppSettingsFileNameTokenConverter, DefaultConfigurationNameToAppSettingsFileNameTokenConverter>()
                 ;
 
             return services;
