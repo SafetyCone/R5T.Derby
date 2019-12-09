f18c968d157a6a2852bd060741206b2f6e4a6f04
MinexAutomation <minex.automation@gmail.com>
2019-12-08 23:29:59 -0800

    Added documentation to application configuration startup and made class abstract.

--- source/R5T.Derby/Code/Startup/ApplicationConfigurationStartup.cs
+++ source/R5T.Derby/Code/Startup/ApplicationConfigurationStartup.cs
@@ -12,13 +12,16 @@ using R5T.Scotia.Extensions;
 
 namespace R5T.Derby
 {
-    public class ApplicationConfigurationStartup : ApplicationConfigurationStartupBase
+    public abstract class ApplicationConfigurationStartup : ApplicationConfigurationStartupBase
     {
         public ApplicationConfigurationStartup(ILogger<ApplicationConfigurationStartup> logger)
             : base(logger)
         {
         }
 
+        /// <summary>
+        /// Adds the default appsettings.json file.
+        /// </summary>
         protected override void ConfigureConfigurationBody(IConfigurationBuilder configurationBuilder)
         {
             configurationBuilder
@@ -26,6 +29,10 @@ namespace R5T.Derby
                 ;
         }
 
+        /// <summary>
+        /// Adds direct configuration-based configuration-name provider and custom user secret files location.
+        /// </summary>
+        /// <param name="services"></param>
         protected override void ConfigureServicesBody(IServiceCollection services)
         {
             services
