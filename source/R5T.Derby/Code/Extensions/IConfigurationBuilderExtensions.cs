2a77a40146b8ad0fc132e149b47c4bf80af02b5e
MinexAutomation <minex.automation@gmail.com>
2019-12-11 12:30:28 -0800

    Added configuration-builder extension that adds a configuration name-specific configuration file using an intermediate configuration to get the configuration name (different from services-based approach).

--- source/R5T.Derby/Code/Extensions/IConfigurationBuilderExtensions.cs
+++ source/R5T.Derby/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -3,9 +3,11 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-using ShrewsburyFileNames = R5T.Shrewsbury.FileNames;
-
+using R5T.Ives.Configuration;
 using R5T.Suebia;
+
+using ShrewsburyFileNames = R5T.Shrewsbury.FileNames;
+using ShrewsburyUtilities = R5T.Shrewsbury.Utilities;
 
 
 namespace R5T.Derby.Extensions
@@ -18,6 +20,23 @@ namespace R5T.Derby.Extensions
         public static IConfigurationBuilder AddDefaultAppSettingsJsonFile(this IConfigurationBuilder configurationBuilder)
         {
             configurationBuilder.AddJsonFile(ShrewsburyFileNames.DefaultAppSettingsJsonFileName);
+
+            return configurationBuilder;
+        }
+
+        public static IConfigurationBuilder AddConfigurationSpecificAppSettingsJsonFile(this IConfigurationBuilder configurationBuilder)
+        {
+            var intermediateConfiguration = configurationBuilder.Build();
+
+            var configurationName = intermediateConfiguration[DirectConfigurationBasedConfigurationNameProvider.ConfigurationNameConfigurationKey];
+
+            // No validation of configuration name nor conversion to appsettings file-name token. Just direct use.
+
+            var configurationNameSpecificAppSettingsJsonFileName = ShrewsburyUtilities.GetConfigurationNameSpecificAppSettingsJsonFileName(configurationName);
+
+            // No directory path specification, just assume configuration file is in executable directory.
+
+            configurationBuilder.AddJsonFile(configurationNameSpecificAppSettingsJsonFileName);
 
             return configurationBuilder;
         }
