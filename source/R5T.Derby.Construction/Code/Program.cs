850ab4250e17ba8c21f5666e881c4ae40d4f5a12
MinexAutomation <minex.automation@gmail.com>
2019-12-13 11:58:12 -0800

    Added tests of configuration-name-based and machine-location-based configuration services functionality.

--- source/R5T.Derby.Construction/Code/Program.cs
+++ source/R5T.Derby.Construction/Code/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Ives;
+using R5T.Richmond;
+using R5T.Suebia;
+
+
 namespace R5T.Derby.Construction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Program.TryApplicationConfigurationConfigurationStartup();
+            Program.TryApplicationConfigurationStartup();
+        }
+
+        private static void TryApplicationConfigurationStartup()
+        {
+            var applicationConfigurationServiceProvider = ApplicationBuilder.New().GetApplicationConfigurationServiceProvider();
+
+            var secretsDirectoryPathProvider = applicationConfigurationServiceProvider.GetRequiredService<ISecretsDirectoryPathProvider>();
+
+            var secretsDirectoryPath = secretsDirectoryPathProvider.GetSecretsDirectoryPath();
+
+            Console.WriteLine($"Secrets directory path: {secretsDirectoryPath}");
+        }
+
+        private static void TryApplicationConfigurationConfigurationStartup()
+        {
+            var applicationConfigurationConfigurationServiceProvider = ApplicationBuilder.New().GetApplicationConfigurationConfigurationServiceProvider();
+
+            var configurationNameProvider = applicationConfigurationConfigurationServiceProvider.GetRequiredService<IConfigurationNameProvider>();
+
+            var configurationName = configurationNameProvider.GetConfigurationName();
+
+            Console.WriteLine($"Configuration name: {configurationName}");
         }
     }
 }
