9e1b32b76b346f385956aca67362015a919d9ffc
MinexAutomation <minex.automation@gmail.com>
2020-06-08 15:21:03 -0700

    Removed AddX() methods for configuration-specific appsetting.json file in favor of the Shrewsbury library methods.

--- source/R5T.Derby/Code/Extensions/IConfigurationBuilderExtensions.cs
+++ source/R5T.Derby/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -3,45 +3,14 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-using R5T.Ives;
-using R5T.Shrewsbury;
 using R5T.Shrewsbury.Extensions;
 using R5T.Suebia;
-
-using ShrewsburyUtilities = R5T.Shrewsbury.Utilities;
 
 
 namespace R5T.Derby.Extensions
 {
     public static class IConfigurationBuilderExtensions
     {
-        public static IConfigurationBuilder AddDefaultAndConfigurationSpecificAppSettingsJsonFiles(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider, bool configurationSpecificIsOptional = false)
-        {
-            configurationBuilder
-                .AddDefaultAppSettingsJsonFile(configurationServiceProvider)
-                .AddConfigurationSpecificAppSettingsJsonFile(configurationServiceProvider, configurationSpecificIsOptional)
-                ;
-
-            return configurationBuilder;
-        }
-
-        public static IConfigurationBuilder AddConfigurationSpecificAppSettingsJsonFile(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider, bool optional = false)
-        {
-            var configurationNameProvider = configurationServiceProvider.GetRequiredService<IConfigurationNameProvider>();
-
-            var configurationName = configurationNameProvider.GetConfigurationName();
-
-            var configurationNameToAppSettingFileTokenConverter = configurationServiceProvider.GetRequiredService<IConfigurationNameToAppSettingsFileNameTokenConverter>();
-
-            var configurationNameAppSettingsFileNameToken = configurationNameToAppSettingFileTokenConverter.ConvertConfigurationNameToAppSettingsFileNameToken(configurationName);
-
-            var configurationNameSpecificAppSettingsFileName = ShrewsburyUtilities.GetConfigurationNameSpecificAppSettingsJsonFileName(configurationNameAppSettingsFileNameToken);
-
-            configurationBuilder.AddAppSettingsDirectoryJsonFile(configurationServiceProvider, configurationNameSpecificAppSettingsFileName, optional);
-
-            return configurationBuilder;
-        }
-
         public static IConfigurationBuilder AddRivetUserSecretsJsonFile(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider, string secretsFileName)
         {
             var secretsFilePathProvider = configurationServiceProvider.GetRequiredService<ISecretsDirectoryFilePathProvider>();
