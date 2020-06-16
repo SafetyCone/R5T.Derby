3eb52816be2a64d5378290ecd6b0304d39f9c890
MinexAutomation <minex.automation@gmail.com>
2020-06-16 16:54:03 -0700

    Needed to adjust Dropbox directory path for new Shared directory.

---
+++ source/R5T.Derby/Code/Services/Implementations/SharedRivetOrganizationDirectoryPathProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using R5T.Alamania;
+using R5T.Bulgaria;
+using R5T.Costobocia;
+using R5T.Lombardy;
+using R5T.Ostrogothia.Rivet;
+
+
+namespace R5T.Derby
+{
+    /// <summary>
+    /// Provides the Rivet organization directory path from the Dropbox directory path (provided by the codenamed Bulgaria project).
+    /// </summary>
+    public class SharedRivetOrganizationDirectoryPathProvider : IRivetOrganizationDirectoryPathProvider
+    {
+        public const string SharedDirectoryName = "Shared"; // Should be in an ISharedDirectoryNameConventions service library.
+
+
+        private IDropboxDirectoryPathProvider DropboxDirectoryPathProvider { get; }
+        private IOrganizationStringlyTypedPathOperator OrganizationStringlyTypedPathOperator { get; }
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public SharedRivetOrganizationDirectoryPathProvider(
+            IDropboxDirectoryPathProvider dropboxDirectoryPathProvider,
+            IOrganizationStringlyTypedPathOperator organizationStringlyTypedPathOperator,
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.DropboxDirectoryPathProvider = dropboxDirectoryPathProvider;
+            this.OrganizationStringlyTypedPathOperator = organizationStringlyTypedPathOperator;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetRivetOrganizationDirectoryPath()
+        {
+            var dropboxDirectoryPath = this.DropboxDirectoryPathProvider.GetDropboxDirectoryPath();
+
+            var rivetOrganizationDropboxDirectoryPath = this.OrganizationStringlyTypedPathOperator.GetOrganizationDirectoryPath(dropboxDirectoryPath, RivetOrganization.Instance.Value);
+
+            var sharedRivetOrganizationDropboxDirectoryPath = this.StringlyTypedPathOperator.GetDirectoryPath(rivetOrganizationDropboxDirectoryPath, SharedRivetOrganizationDirectoryPathProvider.SharedDirectoryName);
+
+            return sharedRivetOrganizationDropboxDirectoryPath;
+        }
+    }
+}
